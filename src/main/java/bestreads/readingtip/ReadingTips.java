547aa0e30d28e297b32bee6dea8ec327273938dc
Janne Tuukkanen <jatufin@gmail.com>
2022-02-27 14:36:51 +0200

    Create Javadocs

--- src/main/java/bestreads/readingtip/ReadingTips.java
+++ src/main/java/bestreads/readingtip/ReadingTips.java
@@ -2,19 +2,38 @@ package bestreads.readingtip;
 
 import java.util.*;
 
+/** The ReadingTips object contains a collection of
+ *  reading tips of Tip class objects.
+ *
+ * @see Tip
+ */
 public class ReadingTips {
+    /** Container for the tips */
     private ArrayList<Tip> tips;
-    
+
+    /** The constructor of the ReadingTips class
+     */
     public ReadingTips() {
 	    this.tips = new ArrayList<Tip>();
     }
 
+    /** Creates new Tip object and adds it to the cllection
+     *
+     * @param url The network adress of the tip
+     * @param title The title text for the tip
+     */
     public void addTip(String url, String title) {
 	    Tip newTip = new Tip(url, title);
 	
 	    this.tips.add(newTip);    
     }
 
+    /** Produces string representation of all the tips.
+     * Each line contains one tip
+     *
+     * @return Tips as a multiline string
+     * @see Tip#toString()
+     */
     // @override
     public String toString() {
 	    String allTips = "";
--- src/main/java/bestreads/readingtip/Tip.java
+++ src/main/java/bestreads/readingtip/Tip.java
@@ -1,10 +1,19 @@
 package bestreads.readingtip;
 
-    
+/** The Tip object contains the data of a single reading tip.
+ * @see ReadingTips class is a container for Tip class objects
+ */
 public class Tip {
+    /** The URL of the tip */
     private String url;
+    /** The tip title */
     private String title;
 
+    /** Constructor for the Tip class
+     *
+     * @param url The network adress of the tip
+     * @param title The title text for the tip
+     */
     public Tip(String url, String title) {
 	    this.url = url;
 	    this.title = title;
@@ -26,6 +35,11 @@ public class Tip {
 	    return this.title;
     }
 
+    /** Converts a Tip to a String
+     * 
+     * @return String reprsentation of the tip in format:
+     *         "Title text : http://tip.address/"
+     */
     // @override
     public String toString() {
 	    return this.title + " : " + this.url;
