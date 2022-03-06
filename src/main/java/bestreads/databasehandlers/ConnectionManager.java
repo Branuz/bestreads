7d172b244a7bf2a079bbc532f85c01715b1be106
ruusukivi <ruusukivi@gmail.com>
2022-03-06 19:22:33 +0200

    checkstyle fixes

--- src/main/java/bestreads/databasehandlers/ConnectionManager.java
+++ src/main/java/bestreads/databasehandlers/ConnectionManager.java
@@ -5,31 +5,34 @@ import java.sql.DriverManager;
 
 public class ConnectionManager {
     private static String defaultFileName = "bestreads.db";
-    
+
     private static Connection con;
 
-    
-    /** Creates a connection between the application and database.
+    /**
+     * Creates a connection between the application and database.
+     * 
      * @return Connection for the database
      * @throws Exception
      */
     public static Connection getConnection() throws Exception {
-	
-	return getConnection(defaultFileName);
-	
+
+        return getConnection(defaultFileName);
+
     }
 
-    /** Creates a connection between the application and database.
+    /**
+     * Creates a connection between the application and database.
+     * 
      * @param fileName The database filename
      *
      * @return Connection for the database
      * @throws Exception
      */
     public static Connection getConnection(String fileName) throws Exception {
-	
-	String url = "jdbc:sqlite:" + fileName;
+
+        String url = "jdbc:sqlite:" + fileName;
         con = DriverManager.getConnection(url);
-	
-        return con;	
+
+        return con;
     }
 }
