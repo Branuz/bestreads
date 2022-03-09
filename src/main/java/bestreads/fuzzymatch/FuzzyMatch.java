d27784c8e3a5745783cc6dbe1df9471c53f520a4
Janne Tuukkanen <jatufin@gmail.com>
2022-03-10 00:35:01 +0200

    Add a class for fuzzy string matching

---
+++ src/main/java/bestreads/fuzzymatch/FuzzyMatch.java
@@ -0,0 +1,153 @@
+package bestreads.fuzzymatch;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/** Static class for matching strings against each other considering
+ *  there might be spelling errors
+ */
+public class FuzzyMatch {
+
+    /** Test if first argument is a substring of the second argument
+     * Example exactMatch("a", "abc") returns True
+     *
+     * @param a String to be searched
+     * @param b String to be searched from
+     *
+     * @return If first string is found with in second, true
+     */
+    private static Boolean exactMatch(String a, String b) {
+	b = b.toLowerCase();
+	a = a.toLowerCase();	
+
+	String regex = a;
+	Pattern pattern = Pattern.compile(regex);
+	Matcher matcher = pattern.matcher(b);
+
+	return matcher.find();
+    }
+
+    /** Test if first argument is a substring of the second argument,
+     *even if the first string is missing one matching character
+     *
+     * Example missingChar("ac", "aaabccc") returns True
+     *
+     * @param a String to be searched
+     * @param b String to be searched from
+     *
+     * @return If first string is found with in second in fuzzy way, true
+     */    
+    private static Boolean missingChar(String a, String b) {
+	b = b.toLowerCase();
+	a = a.toLowerCase();	
+	
+	for(int i=1; i < a.length(); i++) {
+	    String a_left = a.substring(0,i);
+	    String a_right = a.substring(i, a.length());
+	    
+	    String regex = a_left + "." + a_right;
+	    System.out.println("    Regex: " + regex);
+	    Pattern pattern = Pattern.compile(regex);
+
+	    Matcher matcher = pattern.matcher(b);
+	    if(matcher.find()) {
+		return true;
+	    }	
+	}
+	return false;
+    }
+    
+    /** Test if first argument is a substring of the second argument
+     * Example additionalChar("abc", "aaaccc") returns True
+     *
+     * @param a String to be searched
+     * @param b String to be searched from
+     *
+     * @return If first string is found with in second in fuzzy way, true
+     */    
+    private static Boolean additionalChar(String a, String b) {
+	b = b.toLowerCase();
+	a = a.toLowerCase();	
+
+	for(int i=1; i <= a.length(); i++) {
+	    String a_left = a.substring(0,i-1);
+	    String a_right = a.substring(i, a.length());
+	    
+	    String regex = a_left + a_right;
+	    System.out.println("    Regex: " + regex);
+	    Pattern pattern = Pattern.compile(regex);
+
+	    Matcher matcher = pattern.matcher(b);
+	    if(matcher.find()) {
+		return true;
+	    }	
+	}
+
+	return false;
+    }
+
+    /** Test if first argument is a substring of the second argument
+     * Example flippedChar("adc", "aaabccc") returns True
+     *
+     * @param a String to be searched
+     * @param b String to be searched from
+     *
+     * @return If first string is found with in second in fuzzy way, true
+     */    
+    private static Boolean flippedChar(String a, String b) {
+	b = b.toLowerCase();
+	a = a.toLowerCase();	
+	
+	for(int i=1; i <= a.length(); i++) {
+	    String a_left = a.substring(0,i-1);
+	    String a_right = a.substring(i, a.length());
+	    
+	    String regex = a_left + "." + a_right;
+	    System.out.println("    Regex: " + regex);
+	    Pattern pattern = Pattern.compile(regex);
+
+	    Matcher matcher = pattern.matcher(b);
+	    if(matcher.find()) {
+		return true;
+	    }	
+	}
+
+	return false;
+    }
+
+    /** Test if first argument is a substring of the second argument
+     * Example additionalChar("abc", "aaaccc") returns True
+     *
+     * @param a String to be searched
+     * @param b String to be searched from
+     *
+     * @return If first string is found with in second in fuzzy way, true
+     */    
+    public static Boolean fuzzyMatch(String a, String b) {
+	
+	return exactMatch(a, b) ||
+	    missingChar(a, b) ||
+	    additionalChar(a, b) ||
+	    flippedChar(a, b);
+    }
+    public static void main(String[] args) {
+	System.out.println("Fuzzy!");
+
+	String a = "bc";
+	String b = "abcd";
+	System.out.println("A: '" + a + "' B: '"+ b + "' " + fuzzyMatch(a, b));
+
+	a = "bd";
+	b = "abcde";
+	System.out.println("A: '" + a + "' B: '"+ b + "' " + fuzzyMatch(a, b));
+
+	a = "bcd";
+	b = "abde";
+	System.out.println("A: '" + a + "' B: '"+ b + "' " + fuzzyMatch(a, b));
+
+	a = "ba";
+	b = "abcde";
+	System.out.println("A: '" + a + "' B: '"+ b + "' " + fuzzyMatch(a, b));
+	
+    }
+}
